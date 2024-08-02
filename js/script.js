843e10a6b0a4be5ff299decba8fab34c01ba2393
Crowthecruel2 <fanie3vvuuren@gmail.com>
2024-08-02 16:36:47 +0200

    delete all to fix

--- js/script.js
+++
@@ -1,4 +0,0 @@
-
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":980},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-cv","displayName":"CV","link":{"linkType":"LinkTypePage","href":"#!page-cv"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
